ab4e0cb592a30cb587ef0d184e6bcb137456cfe8
i-xi-dev <naoyuki.isono@gmail.com>
2023-07-22 21:09:43 +0900

    2.0.1

--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export { _crypto } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.1.8/mod.ts";
+export { _crypto } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.2.0/mod.ts";
