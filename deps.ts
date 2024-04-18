b2dbc452107bf3f75f17b8f653232c4a8bbdf5d3
i-xi-dev <naoyuki.isono@gmail.com>
2024-04-18 23:47:29 +0900

    3.0.10

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
 export {
   BufferUtils,
   ByteOrder,
-} from "https://raw.githubusercontent.com/i-xi-dev/buffer-utils.es/6.2.2/mod.ts";
-export { _crypto } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.2.17/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/buffer-utils.es/6.2.3/mod.ts";
+export { _crypto } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.2.18/mod.ts";
 export {
   SafeInteger,
   Uint32,
-} from "https://raw.githubusercontent.com/i-xi-dev/number.es/7.3.0/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/number.es/8.0.0/mod.ts";
--- tests/deps.ts
+++ tests/deps.ts
@@ -1,2 +1,2 @@
-export { assertStrictEquals } from "https://deno.land/std@0.215.0/assert/mod.ts";
+export { assertStrictEquals } from "https://deno.land/std@0.223.0/assert/mod.ts";
 export * from "https://deno.land/std@0.160.0/hash/md5.ts";
