6f0dd1f8ba5ccfe3a001b9d4c5b9a21e75924a29
i-xi-dev <naoyuki.isono@gmail.com>
2024-06-17 01:51:50 +0900

    3.0.12

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
 export {
   BufferUtils,
   ByteOrder,
-} from "https://raw.githubusercontent.com/i-xi-dev/buffer-utils.es/6.2.4/mod.ts";
-export { _crypto } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.2.19/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/buffer-utils.es/6.2.5/mod.ts";
+export { _crypto } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.2.20/mod.ts";
 export {
   SafeInteger,
   Uint32,
-} from "https://raw.githubusercontent.com/i-xi-dev/number.es/8.0.1/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/number.es/8.1.0/mod.ts";
